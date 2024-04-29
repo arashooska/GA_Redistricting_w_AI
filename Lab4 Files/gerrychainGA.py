bb59f02f6bdeb6135561f513021e4d3e289cda55
parisacodes <s.parisa.abbasi.gh@gmail.com>
2024-04-29 16:38:31 -0700

    Fixed updaters, Successfully ran chain

--- Lab4 Files/gerrychainGA.py
+++ Lab4 Files/gerrychainGA.py
@@ -21,78 +21,80 @@ from gerrychain.accept import always_accept
 from functools import partial
 import geopandas as gpd
 import time, statistics
+import numpy as np
+from gerrychain.metrics import mean_median, efficiency_gap
 
-def mm(part, election):
+# def mm(part, election):
     
-    """
-    TODO: Calculate the mean-median difference of the districting map corresponding to partition `part`
-    Return the value of the mean-median difference, from the perspective of the Democratic party.
-    """
+#     """
+#     TODO: Calculate the mean-median difference of the districting map corresponding to partition `part`
+#     Return the value of the mean-median difference, from the perspective of the Democratic party.
+#     """
  
-    vote_share_dem = part[election].percents("Democratic")
+#     vote_share_dem = part[election].percents("Democratic")
     
-    mean_vote_share = np.mean(vote_share_dem)
-    median_vote_share = statistics.median(vote_share_dem)
+#     mean_vote_share = np.mean(vote_share_dem)
+#     median_vote_share = statistics.median(vote_share_dem)
 
-    print(f"Mean vote share is {mean_vote_share} and median vote share is {median_vote_share}")
+#     print(f"Mean vote share is {mean_vote_share} and median vote share is {median_vote_share}")
 
-    return median_vote_share - mean_vote_share
+#     return median_vote_share - mean_vote_share
 
 
-def eg(part, election):
-    """
-    TODO: Calculate the efficiency gap of the districting map corresponding to partition `part`, 
-    using the wasted votes definition of the efficiency gap.
-    Return the value of the efficiency gap, from the perspective of the Democratic party.
-    """
+# def eg(part, election):
+#     """
+#     TODO: Calculate the efficiency gap of the districting map corresponding to partition `part`, 
+#     using the wasted votes definition of the efficiency gap.
+#     Return the value of the efficiency gap, from the perspective of the Democratic party.
+#     """
 
-    wv_dem = 0
-    wv_rep = 0
-    all_votes = 0
+#     wv_dem = 0
+#     wv_rep = 0
+#     all_votes = 0
 
-    for d in range(len(part[election].percents("Democratic"))):
-        total_votes = part[election].votes("Democratic")[d] + part[election].votes("Republican")[d]
+#     for d in range(len(part[election].percents("Democratic"))):
+#         total_votes = part[election].votes("Democratic")[d] + part[election].votes("Republican")[d]
 
-        all_votes += total_votes
+#         all_votes += total_votes
 
-        rep_vs = part[election].percents("Republican")[d]
-        dem_vs = part[election].percents("Democratic")[d]
+#         rep_vs = part[election].percents("Republican")[d]
+#         dem_vs = part[election].percents("Democratic")[d]
 
-        #If republicans won, wasted votes for democrats = all democrat votes
-        # Wasted republican votes is every vote over 50%
-        if rep_vs > dem_vs:
-            wv_dem += part[election].votes("Democratic")[d]
-            wv_rep += part[election].votes("Republican")[d] - total_votes / 2
+#         #If republicans won, wasted votes for democrats = all democrat votes
+#         # Wasted republican votes is every vote over 50%
+#         if rep_vs > dem_vs:
+#             wv_dem += part[election].votes("Democratic")[d]
+#             wv_rep += part[election].votes("Republican")[d] - total_votes / 2
         
-        #Else, the opposite
-        else:
-            wv_rep += part[election].votes("Republican")[d]
-            wv_dem += part[election].votes("Democratic")[d] - total_votes / 2
+#         #Else, the opposite
+#         else:
+#             wv_rep += part[election].votes("Republican")[d]
+#             wv_dem += part[election].votes("Democratic")[d] - total_votes / 2
 
-    return (wv_rep - wv_dem) / all_votes
+#     return (wv_rep - wv_dem) / all_votes
 
 
 #Determine number of democratic-won districts for a given partition
-def dem_winning_dists(partition):
+# def dem_winning_dists(partition):
 
-      num_wins = 0
+#       num_wins = 0
 
-      for p_dem in partition["PRES20"].percents("Democratic"):
-            if p_dem > 0.5:
-                  num_wins += 1
+#       for p_dem in partition["PRES20"].percents("Democratic"):
+#             if p_dem > 0.5:
+#                   num_wins += 1
 
-      return num_wins
+#       return num_wins
 
-#Determine number of majority latino districts for a given partition
-def get_num_maj_latino_dists(partition):
+# #Determine number of majority latino districts for a given partition
+# def get_num_maj_latino_dists(partition):
          
-        num_dists = 0
+#         num_dists = 0
     
-        for dist in partition.parts:
-                if partition["dist_pop"][dist] > 0 and partition["dist_latino_pop"][dist]/partition["dist_pop"][dist] > 0.5:
-                    num_dists += 1
+#         for dist in partition.parts:
+#                 if partition["dist_pop"][dist] > 0 and partition["dist_latino_pop"][dist]/partition["dist_pop"][dist] > 0.5:
+#                     num_dists += 1
     
-        return num_dists
+#         return num_dists
 
 # Load in graph from JSON
 # gdf = gpd.read_file("./GA_clean.json")
@@ -105,7 +107,7 @@ ga_graph = Graph.from_json("./GA_clean.json")
 
 #Define constants
 POPULATION_TOLERANCE = 0.02
-NUM_IL_DISTS = 17
+NUM_GA_DISTS = 14
 TOT_POP = sum([ga_graph.nodes()[v]['TOTPOP'] for v in ga_graph.nodes()])
 
 
@@ -114,17 +116,20 @@ start_time = time.time()
 
 
 """
-Elections from json file:
-            "G20PRED": 753,
-            "G20PRER": 62,
-            "G20USSD": 684,
-            "G20USSR": 51,
+Elections from GA_clean.json:
+      "G18GOVRKEM": 193,
+      "G18GOVDABR": 309,
+      "G18ATGRCAR": 206,
+      "G18ATGDBAI": 301,
+      "G18AGRRBLA": 210,
+      "G18AGRDSWA": 295,
 """
 
-# elections = [
-#     Election("PRES20", {"Democratic": "G20PRED", "Republican": "G20PRER"}),
-#     Election("USS20", {"Democratic": "G20USSD", "Republican": "G20USSR"}),
-# ]
+elections = [
+    Election("G18GOV", {"Democratic": "G18GOVDABR", "Republican": "G18GOVRKEM"}),
+    Election("G18ATG", {"Democratic": "G18ATGDBAI", "Republican": "G18ATGRCAR"}),
+    Election("G18AGR", {"Democratic": "G18AGRDSWA", "Republican": "G18AGRRBLA"}),
+]
 
 #Set updaters so that we can make histograms for mean median and efficiency gap, dem/rep winning districts, and cut edges
 #Also marginal box plots
@@ -132,12 +137,11 @@ Elections from json file:
 # Declare updaters to be used per partition in the random walk
 my_updaters = {"population": updaters.Tally("TOTPOP", alias="population"), 
                "cut_edges": cut_edges, 
-               "mean_median": mm,
-                "efficiency_gap": eg,
-               "dist_pop": Tally("TOTPOP", alias="dist_pop"),
-                }
-# election_updaters = {election.name: election for election in elections}
-# my_updaters.update(election_updaters)
+               "district Black": Tally("BVAP", alias="district Black")
+            }
+
+election_updaters = {election.name: election for election in elections}
+my_updaters.update(election_updaters)
 
 # Set up the initial partition object
 initial_partition = Partition(
@@ -150,11 +154,11 @@ initial_partition = Partition(
 random_walk_partial = partial(recom,
                               pop_col = "VAP",
                               epsilon = POPULATION_TOLERANCE,
-                              pop_target=TOT_POP/NUM_IL_DISTS,
+                              pop_target=TOT_POP/NUM_GA_DISTS,
                               node_repeats=2,
                             )
 
-pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, POPULATION_TOLERANCE, pop_key="dist_pop")
+pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, POPULATION_TOLERANCE, pop_key="population")
 
 
 NUM_STEPS = 10
